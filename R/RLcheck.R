9d56a6965b9ced39cc665e8a54720eec43abdad1
Ari Angelo <ari@harmonic.co.nz>
2019-02-26 11:08:11 +1300

    Add RLcheck and build package

---
+++ R/RLcheck.R
@@ -0,0 +1,10 @@
+#' RlCheck
+#'
+#' Check if you are running the right sort of document
+#' @return
+#' @export
+#'
+#' @examples
+RLcheck<-function(){
+  message("You are running the correct github version")
+}
