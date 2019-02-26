0eab167ef0300b5eae0011bc729c881135366386
Ari Angelo <ari@harmonic.co.nz>
2019-02-27 10:15:40 +1300

    Always as double

--- R/RLBigData-classes.r
+++ R/RLBigData-classes.r
@@ -403,17 +403,10 @@ RLBigDataLinkage <- function(dataset1, dataset2, identity1 = NA,
       count = count + 1
     }
 
-    currentLength <- nrow(pairsff)
-    if(currentLength - (2*nfetch) > .Machine$integer.max){
-      currentLength  = as.double(currentLength)
-      message('as.double on count = ', count)
-      browser()
-    }
-    newLength <- currentLength + nrow(slice)
+    currentLength <- as.double(nrow(pairsff))
+    newLength <- currentLength + as.double(nrow(slice))
     if(is.na(newLength)){
       browser()
-    } else {
-      next
     }
     nrow(pairsff) <- newLength
     pairsff[(currentLength + 1):newLength,] <- slice
